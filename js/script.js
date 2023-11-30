a0f7d81e3202da71b777bb325f385ef7ba62dd00
saraaffro <saraaffro@gmail.com>
2023-11-30 15:57:15 +0100

    fix: correct numbers

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 const numbersToRemember = document.getElementById("numeri");
 const time = document.getElementById("tempo");
-
+const writtenNumbers = document.getElementById("numeri-scritti");
+const correctOrNotNumbers = document.getElementById("correct-or-not");
 
 let numbers = [];
 let seconds = 30;
@@ -30,19 +31,37 @@ const timer = setInterval(
             clearInterval(timer);
             // dopo 30 secondi i numeri scompaiono
             numbersToRemember.classList.add("hidden");
-            for (let i= 0; i < 5; i++){
-                // l’utente deve inserire, i numeri che ha visto precedentemente
-                guessNumber = parseInt(prompt("Inserisci uno dei numeri che hai visto prima"));
-                guessNumberArray.push(guessNumber);
-                
-                if(numbers.includes(guessNumberArray[i])){
-                    console.log(`Hai indovinato il numero: ${guessNumberArray[i]}`);
-                } else{
-                    console.log(`Il numero ${guessNumberArray[i]} è sbagliato`);
-                }
-            }
-            console.log("I numeri che hai scritto tu sono: ", guessNumberArray);
-            console.log(`I numeri da indovinare erano: ${numbers}`);
+            time.classList.add("fake-hidden");
+
+                    for (let i= 0; i < 5; i++){
+                        // l’utente deve inserire, i numeri che ha visto precedentemente
+                        setTimeout(waitPrompt, 1000);
+                        function waitPrompt() {
+                            guessNumber = parseInt(prompt("Inserisci un numero che hai visto prima"));
+                            guessNumberArray.push(guessNumber);
+                            console.log(guessNumberArray);
+                        }
+
+                        setTimeout(waitText, 6000);
+                        function waitText(){
+                            if(numbers.includes(guessNumberArray[i])){
+                                console.log(`Hai indovinato il numero: ${guessNumberArray[i]}`);
+                                correctOrNotNumbers.innerHTML += `Hai indovinato il numero: ${guessNumberArray[i]}`;
+                            } else{
+                                console.log(`Il numero ${guessNumberArray[i]} è sbagliato`);
+                            }
+                            // console.log(`I numeri da indovinare erano: ${numbers}`);
+                            numbersToRemember.classList.remove("hidden");
+                             numbersToRemember.innerHTML = `I numeri da indovinare erano: ${numbers}.`
+                            // console.log("I numeri che hai scritto tu sono: ", guessNumberArray);
+                            writtenNumbers.innerHTML = `I numeri che hai scritto tu sono: ${guessNumberArray}.`;
+                            
+                            
+                        }
+                        
+                        
+                        
+                    } 
         } else{
             seconds--;
             // console.log(seconds);
