ab36423c39e3bd34ca3b4f993739cb6de3f62457
saraaffro <saraaffro@gmail.com>
2023-11-30 13:16:22 +0100

    config: create html and js files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+Descrizione:
+Visualizzare in pagina 5 numeri casuali.
+Da lì parte un timer di 30 secondi. Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, i numeri che ha visto precedentemente.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+// Visualizzare in pagina 5 numeri casuali
+
+// far partire un timer di 30 secondi
+
+// dopo 30 secondi i numeri scompaiono 
+
+// l’utente deve inserire, i numeri che ha visto precedentemente
+
+// dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati
